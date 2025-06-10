bfdf056c4f698815107fb5516c82dea0fd0c6860
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2025-06-10 21:21:38 +0700

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, input} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.scss'
+// })
+// export class App {
+//   protected title = 'priorify-angular';
+// }
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-user',
+    template: `
+        <p>The user's name is {{name()}}</p>
+    `,
+})
+export class User {
+    name = input<string>()
+}
+
+@Component({
+    selector: 'app-root',
+    template: `
+        <app-user name="Simran"></app-user>`,
+    imports: [
+        User
+    ]
 })
 export class App {
-  protected title = 'priorify-angular';
+    message = '';
+
+    onMouseOver() {
+        this.message = 'Way to go';
+    }
 }
