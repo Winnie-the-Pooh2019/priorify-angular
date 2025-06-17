9bd6c6e25dd70ed5eea6aeeba045cb476b80daf2
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2025-06-17 21:55:09 +0700

    user: main side bar done

--- src/app/feature/auth/repository/auth.repository.ts
+++ src/app/feature/auth/repository/auth.repository.ts
@@ -1,13 +1,10 @@
 import {LoginRequestDto} from '../dto/login-request.dto';
 import {LoginResponseDto} from '../dto/login-response.dto';
-import {Injectable} from '@angular/core';
-import {MockAuthRepository} from './mock-auth.repository';
-import {RefreshRequestDto} from '../dto/refresh-request.dto';
 
 export abstract class AuthRepository {
     abstract login(loginRequest: LoginRequestDto): Promise<LoginResponseDto>;
 
-    abstract refresh(refreshRequestDto: RefreshRequestDto): Promise<LoginResponseDto>;
+    abstract refresh(): Promise<LoginResponseDto>;
 
     abstract logout(): Promise<void>;
 }
